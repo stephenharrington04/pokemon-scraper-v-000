b5b43920700726df03a3bb28d95023aef2400b2d
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-12 16:26:04 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Pokemon
-  attr_accessor :name, :type, :db, :id
+  attr_accessor :name, :type, :db, :id, :hp
 
   def initialize(id:, name:, type:, db:, hp: nil)
     @id = id
